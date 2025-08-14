2a2808e4617944c4985203cc4f69a752bfa48af6
changvui <changvui12@gmail.com>
2025-08-14 15:15:03 +0800

    test

--- src/control/DoctorManager.java
+++ src/control/DoctorManager.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
  */
 public class DoctorManager {
     public QueueInterface<Doctor> doctorQueue = new LinkedQueue<>();
-    
-    
+   
     
   //-----------------------------------------------------------------------------------------------------------------//  
     public void addDoctor(Doctor doctor) {
